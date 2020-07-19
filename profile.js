c16423631a69eb9f243bb9160a6ce2819a4189d3
Shoaib Jakvani <sjakva3@uic.edu>
2020-07-19 16:41:51 +0000

    updating starter

---
+++ profile.js
@@ -0,0 +1,19 @@
+let profile = {
+    "firstName": "Shoaib",
+    "lastName" : "Jakvani",
+    "address"  : "Romeoville, IL",
+    "phoneNum" : "(312) 532-1938",
+    "email"    : "JakvaniShoaib@gmail.com",
+    "projects" : [
+        {
+            "name": "Confirmed COVID-19 Cases App ",
+            "repoURL": "https://github.com/sjakva/it202-project4",
+            "liveURL": "https://sjakva.github.io/it202-project4/",
+            "description": "Demo of an app that reports confirmed COVID-19 cases per country.",
+            
+        },
+        {
+            "name": "Canvas Scrolling Game",
+        }
+    ]
+}
