a4921d8a5d7ce44c57780f4cfdbd40ca6d7acb5f
Shoaib Jakvani <sjakva3@uic.edu>
2020-07-26 16:59:18 +0000

    updating info

--- js/profile.js
+++ js/profile.js
@@ -6,11 +6,13 @@ let profile = {
     "email"    : "JakvaniShoaib@gmail.com",
     "projects" : [
         {
-            "name": "Confirmed COVID-19 Cases App ",
+            "name": "Confirmed COVID-19 Cases App",
             "repoURL": "https://github.com/sjakva/it202-project4",
             "liveURL": "https://sjakva.github.io/it202-project4/",
-            "description": "Demo of an app that reports confirmed COVID-19 cases per country.",
-            "bulletPoints": ["val1", "val2", "val3"]
+            "description": "Demo of an app that reports confirmed COVID-19 cases per country",
+            "bulletPoints": ["•	Designed an application that fetched and stored COVID-19 data from a given API endpoint",
+                             "•	Developed JavaScript/JQuery code to utilize Material Components for the Web to implement functioning application elements",
+                             "•	Assembled code to loop through stored data objects and output values to table and chart using Google Charts components"]
         },
         {
             "name": "Canvas Scrolling Game",
@@ -21,3 +23,5 @@ let profile = {
     // loop through profile data object and update template
 //console.log(profile['firstName']+ " " + profile['lastName']);
     // easier to modify template for name, address, email, etc than to write complex selectors
+
+// projects
