6132b7d82f38c5434fab012ca81c55e5d6a3c919
Shoaib Jakvani <jakvanishoaib@gmail.com>
2022-11-12 20:55:15 -0600

    gatsby rework: init

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Shoaib's Resume`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-sitemap"]
+};
